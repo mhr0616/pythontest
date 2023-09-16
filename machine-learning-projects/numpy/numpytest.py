42b9c3aa4fc49591e5b575e8a3e6f8e9fda0c7b5
mhr0616 <mhr19552695889@outlook.com>
2023-09-16 15:51:56 +0800

    new level

---
+++ machine-learning-projects/numpy/numpytest.py
@@ -0,0 +1,15 @@
+import numpy  as np
+array_1 =np.array([1,2,3],dtype=np.float64)
+print(array_1)
+
+#查看数据类型
+print(type(array_1))
+
+#二维数据
+array_2 = np.array([[1,2,3],[4,5,6]])
+print(array_2)
+
+#查看他的形状
+print(array_2.shape)
+#查看维度
+print(array_2.ndim)
